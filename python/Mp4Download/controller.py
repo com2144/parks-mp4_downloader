8de888ca07f56a820e7bd7167fd50592025954cd
Park sunjun <com2144@gmail.com>
2023-05-04 15:07:03 +0900

    comfirm result add

--- python/Mp4Download/controller.py
+++ python/Mp4Download/controller.py
@@ -3,32 +3,16 @@
 
 from model import DownLoadModel
 from view import *
+from action_handler import *
 from PySide2.QtWidgets import *
 import sys
 import os
-import urllib.parse
-import logging as logger
-from shotgun_api3 import shotgun
 
 
 class DownLoadController:
-    def __init__(self, main_window, url):
-        # url is variable
-        shotgrid_url = "https://rndtest.shotgrid.autodesk.com/"
-        scripts_name = "script_psj"
-        scripts_key = "qJq9qnmuv*aaxqkulybgdymlr"
+    def __init__(self, main_window, argv):
 
-        self.sg = shotgun.Shotgun(shotgrid_url, script_name=scripts_name, api_key=scripts_key)
-
-        self.log_file = ''
-        self.log_file_setting()
-        self.logger = self._init_log(self.log_file)
-        self.url = url
-        self.protocol, self.action, self.params = self._parse_url()
-
-        self.entity_type = ''
-        self.selected_ids = ''
-        self.selected_ids_filter = []
+        self.action = ActionHandler(argv)
 
         model = DownLoadModel()
         self.main_window = main_window
@@ -41,8 +25,6 @@ class DownLoadController:
         self.browse_sig = False
 
         self.main_ui()
-
-        self.init_set()
 
     def main_ui(self):
         self.view.path_line_edit.setPlaceholderText("Select the save folder")
@@ -61,78 +43,6 @@ class DownLoadController:
         self.view.cancel_button.clicked.connect(self.on_cancel_button_clicked)
 
         self.view.setLayout(self.view.main_vbox_layout)
-
-    @staticmethod
-    def _init_log(filename):
-        try:
-            logger.basicConfig(
-                level=logger.DEBUG,
-                format="%(asctime)s %(levelname)-8s %(message)s",
-                datefmt="%Y-%b-%d %H:%M:%S",
-                filename=filename,
-                filemode="w+",
-            )
-        except IOError as e:
-            raise ShotgunActionException("Unable to open logfile for writing: %s" % e)
-        logger.info("ShotgunAction logging started.")
-        return logger
-
-    def _parse_url(self):
-        logger.info("Parsing full url received: %s" % self.url)
-
-        # get the protocol used
-        protocol, path = self.url.split(":", 1)
-        logger.info("protocol: %s" % protocol)
-
-        # extract the action
-        action, params = path.split("?", 1)
-        action = action.strip("/")
-        logger.info("action: %s" % action)
-
-        # extract the parameters
-        # 'column_display_names' and 'cols' occurs once for each column displayed so we store it as a list
-        params = params.split("&")
-        p = {"column_display_names": [], "cols": []}
-        for arg in params:
-            key, value = map(urllib.parse.unquote, arg.split("=", 1))
-            if key == "column_display_names" or key == "cols":
-                p[key].append(value)
-            else:
-                p[key] = value
-        params = p
-        logger.info("params: %s" % params)
-        return protocol, action, params
-
-    def init_set(self):
-        # entity type that the page was displaying
-        self.entity_type = self.params["entity_type"]
-
-        # ids of entities that were currently selected
-        if len(self.params["selected_ids"]) > 0:
-            sids = self.params["selected_ids"].split(",")
-            self.selected_ids = [int(sid) for sid in sids]
-
-        # All selected ids of the entities returned by the query in filter format ready
-        # to use in a find() query
-        self.selected_ids_filter = self._convert_ids_to_filter(self.selected_ids)
-
-    def log_file_setting(self):
-        log_dir = os.path.dirname(sys.argv[0]) + os.sep + 'action_log'
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
-        else:
-            self.log_file = log_dir + os.sep + 'shotgun_action.log'
-
-    # ----------------------------------------------
-    # Convert IDs to filter format to us in find() queries
-    # ----------------------------------------------
-    @staticmethod
-    def _convert_ids_to_filter(idents):
-        filter_list = []
-        for ident in idents:
-            filter_list.append(["id", "is", ident])
-        logger.debug("parsed ids into: %s" % filter_list)
-        return filter_list
 
     def on_browse_button_clicked(self):
         self.browse_sig = True
@@ -204,31 +114,26 @@ class DownLoadController:
 
     def download_url_file(self):
         version_fields = ["id", "sg_uploaded_movie"]
-        for sid in self.selected_ids_filter:
-            version = self.sg.find_one(self.entity_type, [sid], version_fields)
+        for sid in self.action.selected_ids_filter:
+            version = self.action.sg.find_one(self.action.entity_type, [sid], version_fields)
             mp4_down = self.path + '/' + version["sg_uploaded_movie"]["name"]
             mp4_filter = mp4_down.split('.')
             low_ext = mp4_filter[-1].lower()
             if low_ext != 'mp4':
                 pass
             else:
-                self.sg.download_attachment(version["sg_uploaded_movie"], file_path=mp4_down)
-
-
-class ShotgunActionException(Exception):
-    pass
+                self.action.sg.download_attachment(version["sg_uploaded_movie"], file_path=mp4_down)
 
 
 def main():
     app = QApplication(sys.argv)
     window = QMainWindow()
     try:
-        controller = DownLoadController(window, sys.argv[1])
-        logger.info("ShotgunAction: Firing... %s" % (sys.argv[1]))
+        controller = DownLoadController(window, sys.argv)
+        controller.action.log.info("ShotgunAction: Firing... %s" % (sys.argv[1]))
     except IndexError as e:
         raise ShotgunActionException("Missing GET arguments")
-    logger.info("ShotgunAction process finished.")
-
+    controller.action.log.info("ShotgunAction process finished.")
     window.setCentralWidget(controller.view)
     window.setWindowTitle('Mp4 Downloader')
     window.setFixedSize(500, 150)
